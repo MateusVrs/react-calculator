7ce18ee9dc59e64dfb4aff7b46193079abb00058
Mateus Vieira <matucavieira@gmail.com>
2021-10-29 18:01:14 -0300

    react lifting state up

--- src/App.js
+++ src/App.js
@@ -2,21 +2,65 @@ import React from "react";
 
 function Button(props) {
   return (
-    <button className="btn" onClick={props.onClick}>
+    <button className="btn" onClick={(e) => props.onClick(e.target.innerText)}>
       {props.value}
     </button>
+  );
+}
+
+function InputItem(props) {
+  return (
+    <section className="input-container">
+      <input id="calculation-text" type="text" value={props.insertValue} onChange={(e) => props.onChange(e.target.value)} />
+    </section>
   );
 }
 
 class ButtonBox extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      inputValue: ''
+    this.numbersArray = props.numbersArray;
+    this.signalsArray = props.signalsArray;
+    this.addChar = props.addChar;
+    this.doCalculation = props.doCalculation;
+  }
+
+  render() {
+    const btn_numbers = [];
+
+    for (let index = 0; index < 7; index = index + 3) {
+      btn_numbers.push(
+        <div key={index / 3} className="btn-row">
+          {this.numbersArray.slice(index, index + 3).reverse().map(
+            (value) => <Button key={value} value={value} onClick={this.addChar} />)
+          }
+          <Button value={this.signalsArray[index / 3]} onClick={this.addChar} />
+        </div>
+      );
     }
 
+    return (
+      <div className="btn-container">
+        {btn_numbers}
+        <div className="btn-row">
+          <Button value={0} onClick={this.addChar} />
+          <Button value={this.signalsArray[4]} onClick={this.doCalculation} />
+          <Button value={this.signalsArray[3]} onClick={this.addChar} />
+        </div>
+      </div>
+    );
+  }
+}
+
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: ''
+    };
+
     this.numbersArray = Array.from(Array(10).keys()).slice(1, 10).reverse();
-    this.someSignals = ['/', '*', '-'];
+    this.signalsArray = ['/', '*', '-', '+', '='];
 
     this.onToDoChange = this.onToDoChange.bind(this);
     this.onToAddChar = this.onToAddChar.bind(this);
@@ -42,44 +86,12 @@ class ButtonBox extends React.Component {
   }
 
   render() {
-    const btn_numbers = [];
-    for (let index = 0; index < 3; index++) {
-      btn_numbers.push(
-        <div key={index} className="btn-row">
-          {
-            this.numbersArray.slice(index * 3, index * 3 + 3).reverse().map(
-              (value) => <Button key={value} value={value} onClick={(e) => this.onToAddChar(e.target.innerText)} />)
-          }
-          <Button value={this.someSignals[index]} onClick={(e) => this.onToAddChar(e.target.innerText)} />
-        </div>
-      );
-    }
+    const value = this.state.inputValue;
 
-    return (
-      <div className="btn-container">
-        <section className="input-container">
-          <input id="calculation-text" type="text" value={this.state.inputValue} onChange={(e) => this.onToDoChange(e.target.value)} />
-        </section>
-        {btn_numbers}
-        <div className="btn-row">
-          <Button value={0} />
-          <Button value={'='} onClick={() => this.doCalculation()}/>
-          <Button value={'+'} onClick={(e) => this.onToAddChar(e.target.innerText)}/>
-        </div>
-      </div>
-    );
-  }
-}
-
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
     return (
       <div className="calculator">
-        <ButtonBox />
+        <InputItem insertValue={value} onChange={this.onToDoChange} />
+        <ButtonBox signalsArray={this.signalsArray} numbersArray={this.numbersArray} addChar={this.onToAddChar} doCalculation={() => this.doCalculation()} />
       </div>
     );
   }
